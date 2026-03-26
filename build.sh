1b12fa26d90cb9910fdc7ecfe7d001ec094c0fff
Mr. Bright <brightakpla2@gmail.com>
2026-03-26 20:33:20 +0000

    try again

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations --no-input
+python manage.py makemigrations dashboard --no-input
 python manage.py migrate --no-input
