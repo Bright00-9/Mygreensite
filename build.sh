60b84ecd3393c07d7a39a8f48a0fa9fe8846d14a
Mr. Bright <brightakpla2@gmail.com>
2026-03-27 09:24:50 +0000

    rebuild the build

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py migrate --fake dashboard zero --no-input
-
 python manage.py makemigrations dashboard --no-input
-python manage.py migrate --no-input
+
+python manage.py migrate dashboard --fake-initial --no-input
 python manage.py collectstatic --no-input
