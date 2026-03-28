6417a344a30a8a39ffead9af2304c2f0939fd380
Mr. Bright <brightakpla2@gmail.com>
2026-03-28 14:23:53 +0000

    celery 3

--- greensite/celery.py
+++ greensite/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'komado_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greensite.settings')
 
 app = Celery('greensite')
 
