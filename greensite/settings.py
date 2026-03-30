970744778d8c3d93a8ef7fa43ff1aa34086478e8
Mr. Bright <brightakpla2@gmail.com>
2026-03-30 17:36:07 +0000

    another corr

--- greensite/settings.py
+++ greensite/settings.py
@@ -69,10 +69,7 @@ WSGI_APPLICATION = 'greensite.wsgi.application'
 # Check if DATABASE_URL is set (it will be on Render, but maybe not on your phone)
 if os.environ.get('DATABASE_URL'):
     DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
-            ssl_require=True
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True
         )
     }
 else:
