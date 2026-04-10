a0f458fa24ba2aeb06799aa6f12ce133d9f829c0
Mr. Bright <brightakpla2@gmail.com>
2026-04-10 19:50:21 +0000

    God help

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py makemigrations dashboard --no-input
-python manage.py django_celery_beat --fake
+
 python manage.py migrate dashboard --fake-initial --no-input
 python manage.py collectstatic --no-input
